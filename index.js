7b8bb064599b3f80766c581a9f3e6f9d9126241a
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-27 02:22:57 +0200

    created functions to show library

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let myLibrary = [bookOne, bookTwo];
+bookZero = new Book("Think and Grow Rich","Napoleon Hill", 276, "yes")
+
+bookOne = new Book("The Master Key System","Charles Haanel", 140, "no")
+
+let myLibrary = [bookZero];
+
+const library = document.querySelector("#library");
+
+const addBookBtn = document.querySelector("#addBook");
+
+addBookBtn.addEventListener("click", addBookBtn.setAttribute("class", "form"))
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,27 +22,42 @@ function Book(title, author, pages, read) {
 }
 
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+function addBook(...libro) {
+    myLibrary.push(...libro);
+    showBook(libro);
 };
 
-function showBook() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = document.createElement("div");
-        let body = document.querySelector("#container");
-        book.setAttribute("id",`book-${i}`);
-        book.setAttribute("class", "book");
-        body.appendChild(book);
+function showBook(libro) {
+    let book = document.createElement("div");
+    let title = document.createElement("title");
+    let author = document.createElement("author");
+    let pages = document.createElement("pages");
+    let read = document.createElement("read");
+    book.setAttribute("id",`book-${myLibrary.length}`);
+    book.setAttribute("class", "book");
+    title.textContent = myLibrary[myLibrary.length -1].title;
+    author.textContent = myLibrary[myLibrary.length -1].author;
+    pages.textContent = myLibrary[myLibrary.length -1].pages;
+    read.textContent = myLibrary[myLibrary.length -1].read;
+    library.appendChild(book);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
+}
+
+function showLibrary() {
+    for (let count = 0; count < myLibrary.length; count++) {
+        showBook();
     }
 };
 
+showLibrary();
 
-bookOne = new Book("Think and Grow Rich","Napoleon Hill", 276, "yes")
-
-bookTwo = new Book("The Master Key System","Charles Haanel", 140, "no")
+addBook(bookOne)
 
 
-console.table(bookOne)
+console.table(myLibrary)
 
 console.log(bookOne.info())
 
