e3cd2e034bf3b8b4796c18cbd1e78fbb70a047ea
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-27 02:50:15 +0200

    made sure showBook() sets correct id's

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ let myLibrary = [bookZero];
 
 const library = document.querySelector("#library");
 
-const addBookBtn = document.querySelector("#addBook");
+const newBookBtn = document.querySelector("#addBook");
 
-addBookBtn.addEventListener("click", addBookBtn.setAttribute("class", "form"))
+newBookBtn.addEventListener("click", newBookBtn.setAttribute("class", "form"))
 
 
 function Book(title, author, pages, read) {
@@ -29,16 +29,27 @@ function addBook(...libro) {
 
 function showBook(libro) {
     let book = document.createElement("div");
-    let title = document.createElement("title");
-    let author = document.createElement("author");
-    let pages = document.createElement("pages");
-    let read = document.createElement("read");
     book.setAttribute("id",`book-${myLibrary.length}`);
     book.setAttribute("class", "book");
+
+    let title = document.createElement("div");
+    title.setAttribute("class", "title");
+
+    let author = document.createElement("author");
+    author.setAttribute("class", "author");
+
+    let pages = document.createElement("pages");
+    pages.setAttribute("class", "pages");
+
+    let read = document.createElement("read");
+    read.setAttribute("class", "read");
+
+
     title.textContent = myLibrary[myLibrary.length -1].title;
     author.textContent = myLibrary[myLibrary.length -1].author;
     pages.textContent = myLibrary[myLibrary.length -1].pages;
     read.textContent = myLibrary[myLibrary.length -1].read;
+    
     library.appendChild(book);
     book.appendChild(title);
     book.appendChild(author);
@@ -49,7 +60,7 @@ function showBook(libro) {
 function showLibrary() {
     for (let count = 0; count < myLibrary.length; count++) {
         showBook();
-    }
+    };
 };
 
 showLibrary();
